9339e9357addd1bec5a58b05b04c984f1ad39652
Nzube Ifediba <ifedibazube@gmail.com>
2022-03-02 20:03:10 +0400

    code

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,11 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
-
   def index
-    # On récupère tout les restaurants
     @restaurants = Restaurant.all
   end
 
   def show
-    # On récupère 1 seul restaurant
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
   end
 
   def new
@@ -15,32 +13,17 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    @restaurant = Restaurant.new(params_restaurant)
-    @restaurant.save
-    redirect_to restaurants_path
-  end
-
-  def edit
-  end
-
-  def update
-    @restaurant.update(params_restaurant)
-    @restaurant.save
-    redirect_to restaurants_path
-  end
-
-  def destroy
-    @restaurant.destroy
-    redirect_to restaurants_path
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render "new"
+    end
   end
 
   private
 
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:id])
-  end
-
-  def params_restaurant
-    params.require(:restaurant).permit(:name, :city, :rating)
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,19 +1,6 @@
 class Restaurant < ApplicationRecord
-  categories = [
-    'chinese',
-    'italian',
-    'japanese',
-    'french',
-    'belgian'
-  ]
-  validates :name, presence: true
-  validates :address, presence: true
-  validates :category, inclusion: { in: categories }
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :name, :address, presence: :true
+  validates :category, inclusion: { in: CATEGORIES }
+  has_many :reviews, dependent: :destroy
 end
-
-# A restaurant must have a name, an address and a category.
-# A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
-# When a restaurant is destroyed, all of its reviews must be destroyed as well.
-# A review must belong to a restaurant.
-# A review must have content and a rating.
-# A review’s rating must be a number between 0 and 5.
